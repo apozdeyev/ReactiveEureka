a9600c7cfa7723a90516c355e9d89c472f1ec01e
AnatoliyPozdeyev <anatoliy.pozdeyev@gmail.com>
2017-11-05 15:36:19 +0300

    Framework project file.

---
+++ ReactiveEureka.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveEureka.h
+//  ReactiveEureka
+//
+//  Created by Anatoliy Pozdeyev on 05/11/2017.
+//  Copyright © 2017 VitaSw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReactiveEureka.
+FOUNDATION_EXPORT double ReactiveEurekaVersionNumber;
+
+//! Project version string for ReactiveEureka.
+FOUNDATION_EXPORT const unsigned char ReactiveEurekaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveEureka/PublicHeader.h>
+
+
